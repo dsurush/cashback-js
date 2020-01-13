2a0d634b518c9697b5e268f61c27202a06d34303
Surush <davlatzoda.surush@gmail.com>
2020-01-13 09:21:30 +0500

    fixed #7

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,19 @@ const SIMPLE_CASHBACK_PERCENT = 1;
 const INCREASED_CASHBACK_PERCENT = 5;
 const SPECIAL_CASHBACK_PERCENT = 30;
 const BORDER_CASHBACK = 3000;
-let cashbackOfOne = 0;
+let predictCashback = 0;
 
 for(const purchase of purchases){
     if(purchase.category == SIMPLE){
-        cashbackOfOne += take_percent(purchase.amount, SIMPLE_CASHBACK_PERCENT);
+        predictCashback += take_percent(purchase.amount, SIMPLE_CASHBACK_PERCENT);
     } else if(purchase.category == INCREASED){
-        cashbackOfOne += take_percent(purchase.amount, INCREASED_CASHBACK_PERCENT);
+        predictCashback += take_percent(purchase.amount, INCREASED_CASHBACK_PERCENT);
     } else if(purchase.category == SPECIAL){
-        cashbackOfOne += take_percent(purchase.amount, SPECIAL_CASHBACK_PERCENT);
+        predictCashback += take_percent(purchase.amount, SPECIAL_CASHBACK_PERCENT);
     }
     
 }
-if(cashbackOfOne > BORDER_CASHBACK)
-    cashbackOfOne = BORDER_CASHBACK;
-cashback = cashbackOfOne;
+if(predictCashback > BORDER_CASHBACK)
+    predictCashback = BORDER_CASHBACK;
+cashback = predictCashback;
 console.log(cashback);
